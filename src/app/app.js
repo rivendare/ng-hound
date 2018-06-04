e02bd786e513c1caf123b8de541801fa35eb8e90
Nav Samra <navsamra@me.com>
2018-06-04 14:35:06 -0700

    MASTER - initial

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,6 @@
 import angular from 'angular';
 
-import '../style/app.css';
+import '../style/app.scss';
 
 let app = () => {
   return {
--- webpack.config.js
+++ webpack.config.js
@@ -95,7 +95,7 @@ module.exports = function makeWebpackConfig() {
       //
       // Reference: https://github.com/postcss/postcss-loader
       // Postprocess your css with PostCSS plugins
-      test: /\.css$/,
+      test: /\.scss$/,
       // Reference: https://github.com/webpack/extract-text-webpack-plugin
       // Extract css files in production builds
       //
@@ -106,7 +106,8 @@ module.exports = function makeWebpackConfig() {
         fallbackLoader: 'style-loader',
         loader: [
           {loader: 'css-loader', query: {sourceMap: true}},
-          {loader: 'postcss-loader'}
+          {loader: 'postcss-loader'},
+          {loader: 'sass-loader'}
         ],
       })
     }, {
