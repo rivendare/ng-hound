f2c8b8b4b1e11aff0091102f05a813d99af73577
Nav Samra <navsamra@me.com>
2018-06-18 11:43:00 -0700

    blah

---
+++ src/app/shitty.js
@@ -0,0 +1,26 @@
+import angular from 'angular'
+
+import '../style/app.scss';
+
+let app = () => {
+  return {
+    template: require('./app.html'),
+    controller: 'AppCtrl',
+    controllerAs: 'app'
+  }
+};
+
+class AppCtrl {
+  constructor() {
+    console.log('blah')
+    this.url = 'https://github.com/preboot/angular-webpack';
+  }
+}
+
+const MODULE_NAME = 'app';
+
+angular.module(MODULE_NAME, [])
+  .directive('app', app)
+    .controller('AppCtrl', AppCtrl)
+
+export default MODULE_NAME;
